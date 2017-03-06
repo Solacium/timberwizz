fc4ed453d49a7a13cb4b06286830a61cd6dd3748
Max Minin <m.minin@rbkmoney.com>
2017-03-06 11:43:00 +0300

    refactoring js logic

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var dropDownShown = false;
+    var currentDropdown;
 
     function initDropdowns() {
         var elements = document.querySelectorAll('.dropdownParent');
@@ -8,37 +8,28 @@
         for (var i=0; i< elements.length; i++) {
             elements[i].onclick = (function (elem) {
                 return function() {
-                    var child = document.getElementById(elem.dataset['target']);
+                    if (currentDropdown) {
+                        hideCurrentDropdown();
+                    }
 
-                    child.style.display = 'block';
-                    setTimeout(function() {
-                        dropDownShown = true;
-                    }, 100);
+                    currentDropdown = document.getElementById(elem.dataset['target']);
+
+                    currentDropdown.style.display = 'block';
                 }
             })(elements[i]);
         }
     }
 
-    function hideDropdowns() {
-        var elements = document.querySelectorAll('.dropdownChild');
-
-        for (var i=0; i< elements.length; i++) {
-            elements[i].style.display = 'none';
-        }
-
-        setTimeout(function() {
-            dropDownShown = false;
-        }, 100);
+    function hideCurrentDropdown() {
+        currentDropdown.style.display = 'none';
     }
 
     function initHide() {
         document.body.addEventListener('click', function(event) {
-            if (event.target.className.indexOf('dropdownChild') === -1) {
-                if (event.target.className.indexOf('dropdownParent') === -1) {
-                    hideDropdowns();
-                } else if (event.target.className.indexOf('dropdownParent') !== -1 && dropDownShown) {
-                    hideDropdowns();
-                }
+            if (event.target !== currentDropdown &&
+                event.target.className.indexOf('dropdownParent') === -1 &&
+                event.target.className.indexOf('dropdownInner') === -1) {
+                hideCurrentDropdown();
             }
         })
     }
